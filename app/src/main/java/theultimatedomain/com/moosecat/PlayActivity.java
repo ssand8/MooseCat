dc6f2badbf4d1a66e5cbf8a8ede04eb7f290086a
STUDENT <student6@STEMs-MacBook-Air-3.local>
2016-04-19 18:01:18 -0600

    yeah

--- app/src/main/java/theultimatedomain/com/moosecat/PlayActivity.java
+++ app/src/main/java/theultimatedomain/com/moosecat/PlayActivity.java
@@ -1,7 +1,10 @@
 package theultimatedomain.com.moosecat;
 
+import android.support.v7.app.AppCompatActivity;
+
 /**
  * Created by student6 on 4/19/16.
  */
-public class PlayActivity {
+public class PlayActivity extends AppCompatActivity {
+
 }
