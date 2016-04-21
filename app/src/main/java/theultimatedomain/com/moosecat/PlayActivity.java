8ea19a552abad8703fe5c7d7247a5b16c6067394
STUDENT <student6@STEMs-MacBook-Air-3.local>
2016-04-21 16:20:34 -0600

    yeah

--- app/src/main/java/theultimatedomain/com/moosecat/PlayActivity.java
+++ app/src/main/java/theultimatedomain/com/moosecat/PlayActivity.java
@@ -4,6 +4,7 @@ import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.widget.ImageView;
+import android.widget.Toast;
 
 /**
  * Created by student6 on 4/19/16.
@@ -17,6 +18,32 @@ public class PlayActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_play);
 
+        setupUI();
+        setupClickListeners();
+    }
 
+    private void setupUI()
+    {
+        mMooseCat = (ImageView) findViewById(R.id.moose_cat_img);
+
+    }
+
+    private void setupClickListeners()
+    {
+        mMooseCat.setOnTouchListener(new OnSwipeTouchListener(PlayActivity.this) {
+            public void onSwipeTop() {
+                Toast.makeText(PlayActivity.this, "top", Toast.LENGTH_SHORT).show();
+            }
+            public void onSwipeRight() {
+                Toast.makeText(PlayActivity.this, "right", Toast.LENGTH_SHORT).show();
+            }
+            public void onSwipeLeft() {
+                Toast.makeText(PlayActivity.this, "left", Toast.LENGTH_SHORT).show();
+            }
+            public void onSwipeBottom() {
+                Toast.makeText(PlayActivity.this, "bottom", Toast.LENGTH_SHORT).show();
+            }
+
+        });
     }
 }
