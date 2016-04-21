d878d1baf370d1215a40b1cea7f2f6b9d2eaa665
STUDENT <student6@STEMs-MacBook-Air-3.local>
2016-04-21 16:08:36 -0600

    yeah

--- app/src/main/java/theultimatedomain/com/moosecat/MainActivity.java
+++ app/src/main/java/theultimatedomain/com/moosecat/MainActivity.java
@@ -1,16 +1,25 @@
 package theultimatedomain.com.moosecat;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button mPlay;
+    private Button mOptions;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        setupUI();
+        setupClickListeners();
 
         //this is simon
 
@@ -19,4 +28,28 @@ public class MainActivity extends AppCompatActivity {
         //this is Joe Buenning who sucks
     }
     //THIS IS SPARTA
+
+    private void setupUI()
+    {
+        mPlay = (Button) findViewById(R.id.play_btn);
+        mOptions = (Button) findViewById(R.id.options_btn);
+    }
+
+    private void setupClickListeners()
+    {
+        mPlay.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, PlayActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        mOptions.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+    }
 }
