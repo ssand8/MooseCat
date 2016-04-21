bde74685477c55ab5896af561f7f8b7c09879959
STUDENT <student6@STEMs-MacBook-Air-3.local>
2016-04-21 16:13:05 -0600

    yeah

--- app/src/main/java/theultimatedomain/com/moosecat/PlayActivity.java
+++ app/src/main/java/theultimatedomain/com/moosecat/PlayActivity.java
@@ -1,10 +1,22 @@
 package theultimatedomain.com.moosecat;
 
+import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.ImageView;
 
 /**
  * Created by student6 on 4/19/16.
  */
 public class PlayActivity extends AppCompatActivity {
 
+    private ImageView mMooseCat;
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_play);
+
+
+    }
 }
